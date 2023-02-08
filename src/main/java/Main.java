34b1d6379f98e86a6c858ea08d6a45815b2b200a
Loi Tran <loitran454@gmail.com>
2023-02-08 00:25:43 -0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    // create a string representation of array it receives as the parameter and return it
+    public static String arrayAsString(int[][] array) {
+        
+    }
+}
