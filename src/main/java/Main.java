7047d5b48c7cb6e8266ea7d307636617e06f8ce3
Loi Tran <loitran454@gmail.com>
2023-02-08 04:08:37 -0800

    Updated arrayAsString() method missing features: line break

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,37 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        int rows = 2;
+//        int columns = 3;
+//        int[][] matrix = new int[rows][columns];
+//        matrix[0][1] = 5;
+//        matrix[1][0] = 3;
+//        matrix[1][2] = 7;
+//        System.out.println(arrayAsString(matrix));
 
+
+        int[][] matrix = {
+                {3, 2, 7, 6},
+                {2, 4, 1, 0},
+                {3, 2, 1, 0}
+        };
+
+        System.out.println(arrayAsString(matrix));
     }
 
-    // create a string representation of array it receives as the parameter and return it
+    // create a string representation of array it receives as the parameter
+    // return string
     public static String arrayAsString(int[][] array) {
-        
+        StringBuilder string = new StringBuilder();
+
+        // iterate through the size of array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                // values in each row of the array
+                string.append(array[i][j]);
+            }
+        }
+        return string.toString();
+
     }
 }
